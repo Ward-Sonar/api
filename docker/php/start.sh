3420640d7c0df68dc9aacdbc01e3270532b593d1
Stuart Laverick <appsol@gmail.com>
2021-02-16 12:22:20 +0000

    Local dev environment set up

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,10 +2,9 @@
 
 # Run PHP-FPM as current user
 if [ ! -z "$WWWUID" ]; then
-    sed -i "s/user\ \=.*/user\ \= $WWWUID/g" /etc/php/7.4/fpm/pool.d/www.conf
 
-    # Set UID of user "www"
-    usermod -u $WWWUID www
+    # Set UID of user "www-data"
+    usermod -u $WWWUID www-data
 fi
 
 # Ensure /.composer exists and is writable
@@ -18,7 +17,7 @@ chmod -R ugo+rw /.composer
 # Run a command or supervisord
 if [ $# -gt 0 ]; then
     # If we passed a command, run it instead
-    exec gosu www "$@"
+    exec gosu www-data "$@"
 else
     # Otherwise start supervisord
     /usr/bin/supervisord
