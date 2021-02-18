06b6b5f15f6653dd38c751ccc2742e0b69aac6b9
Stuart Laverick <appsol@gmail.com>
2021-02-18 11:09:46 +0000

    Attempt to resolve composer permission issue

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,20 +2,25 @@
 
 # Run PHP-FPM as current user
 if [ ! -z "$WWWUID" ]; then
-
+    echo "Set www-data UID to $WWWUID"
     # Set UID of user "www-data"
     usermod -u $WWWUID www-data
 fi
 
 # Ensure /.composer exists and is writable
 if [ ! -d /.composer ]; then
+    echo "Composer home before $COMPOSER_HOME"
+    echo "Create .composer directory in $PWD"
     mkdir /.composer
+    export COMPOSER_HOME=${PWD}/.composer
+    echo "Composer home after $COMPOSER_HOME"
 fi
 
 chmod -R ugo+rw /.composer
 
 # Run a command or supervisord
 if [ $# -gt 0 ]; then
+    echo "Running $@ as www-data"
     # If we passed a command, run it instead
     exec gosu www-data "$@"
 else
