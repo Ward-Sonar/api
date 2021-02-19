db0a520fe628bf6253eebf51489f50110757074f
Stuart Laverick <appsol@gmail.com>
2021-02-19 14:54:43 +0000

    Added first OpenApi comments

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,6 +9,31 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="WardSonar API",
+     *      description="API for wardsonar.co.uk",
+     *      @OA\Contact(
+     *          email="mike@ayup.agency"
+     *      ),
+     *     @OA\License(
+     *         name="Apache 2.0",
+     *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *     )
+     * )
+     *
+     *  @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description=""
+     *  )
+     *
+     *  @OA\Tag(
+     *     name="WardSonar",
+     *     description="API Endpoints for wardsonar.co.uk",
+     * )
+     */
+
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'WardSonar API',
             ],
 
             'routes' => [
@@ -196,8 +196,8 @@ return [
         /*
          * Uncomment to add constants which can be used in annotations
          */
-        // 'constants' => [
-        // 'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
-        // ],
+        'constants' => [
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+        ],
     ],
 ];
